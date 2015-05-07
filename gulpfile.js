aabd0716027754830850d20a103e796a01d37643
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2015-05-07 23:30:21 +0300

    Implement SC5-styleguide

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var pkg = require('./package.json');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var styleguide = require('sc5-styleguide');
 var reload = browserSync.reload;
 
 var config = {
@@ -12,6 +13,9 @@ var config = {
   dist: pkg.config.dist,
   port: pkg.config.port,
   hostname: process.env.HOSTNAME || pkg.config.hostname,
+  patternlibrarypath: pkg.config.patternlibrarypath,
+  tmpPath: "tmp",
+  styleguideTmpPath: "tmp/styleguide",
   sassoptions: {
       sourcemap: true,
       style: "expanded"
@@ -29,37 +33,69 @@ gulp.task('css', function () {
 });
 
 gulp.task('images', function () {
-  return gulp.src(config.source + 'images/**.**')
+  return gulp
+    .src(config.source + 'images/**.**')
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src(config.source + 'js/*.js')
+  return gulp
+    .src(config.source + 'js/*.js')
     .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('fonts', function () {
-  return gulp.src(config.source + 'fonts/*.**')
+  return gulp
+    .src(config.source + 'fonts/*.**')
     .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('html', function () {
-  return gulp.src(config.source + '*.html')
+  return gulp
+    .src(config.source + '*.html')
     .pipe(gulp.dest('dist'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch(config.source + 'js/**', ['scripts']);
-  gulp.watch(config.source + 'css/**/*.scss', ['css']);
-  gulp.watch(config.source + '*.html', ['html', 'css', 'scripts']);
-  gulp.watch(config.source + 'images/**', ['images']);
+// Styleguide stuff
+gulp.task('styleguide:generate', function() {
+  return gulp
+    .src(config.source + 'css/**/*.scss')
+    .pipe(styleguide.generate({
+        title: 'Jalo Patternlibrary',
+        server: true,
+        rootPath: config.styleguideTmpPath,
+        overviewPath: "readme-patternlibrary.md"
+      }))
+    .pipe(gulp.dest(config.styleguideTmpPath));
+});
 
+gulp.task('styleguide:applystyles', function() {
+  return gulp
+    .src(config.source + 'css/import.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(config.styleguideTmpPath));
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+// Watch Files For Changes
+gulp.task('dev', ['html', 'css', 'images', 'scripts', 'styleguide'],function() {
+  gulp.watch(config.source + 'js/**', ['scripts']);
+  gulp.watch(config.source + 'css/**/*.scss', ['css', 'styleguide']);
+  gulp.watch(config.source + '*.html', ['html']);
+  gulp.watch(config.source + 'images/**', ['images']);
+  console.log(
+    '\nDeveloper mode!\n\nJalo Pattern library available at http://localhost:3000/\n'
+  );
   browserSync.init({
     server: config.dist,
     port: config.port
   });
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['dev']);
 gulp.task('deploy', ['css', 'images', 'scripts', 'fonts', 'html']);
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
