12d73641cc5f7085d416f1da1cc27fe5553be9c3
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2015-05-23 16:41:50 +0300

    A lot of fixes

--- gulp/tasks/styleguide.js
+++ gulp/tasks/styleguide.js
@@ -2,19 +2,25 @@ var gulp        = require('gulp');
 var styleguide  = require('sc5-styleguide');
 var sass        = require('gulp-sass');
 var config      = require('../config');
+var handleErrors = require('../util/handleErrors');
 
 gulp.task('styleguide:generate', function() {
   return gulp
-    .src(config.sass.src)
+    .src(config.styleguide.src)
+        .on('error', handleErrors)
     .pipe(styleguide.generate(config.styleguide.config))
+    .on('error', handleErrors)
     .pipe(gulp.dest(config.styleguide.tmp));
 });
 
 gulp.task('styleguide:applystyles', function() {
   return gulp
-    .src(config.sass.dest + 'main.css')
+    .src(config.styleguide.dest)
+    .on('error', handleErrors)
     .pipe(sass(config.sass.settings))
+    .on('error', handleErrors)
     .pipe(styleguide.applyStyles())
+    .on('error', handleErrors)
     .pipe(gulp.dest(config.styleguide.tmp));
 });
 
