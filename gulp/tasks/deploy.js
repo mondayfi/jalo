550411c35385349541236d9eed404ff552aaf3db
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2015-05-26 21:07:25 +0300

    added commands to deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -2,5 +2,5 @@ var gulp 				= require('gulp');
 var runSequence 		= require('run-sequence');
 
 gulp.task('deploy', function(cb){
-   runSequence('sass', 'styleguide', 'vendorstyles', 'images', 'markup', 'html', 'browserify', 'server', cb);
+   runSequence('iconFont', 'favicons', 'sass', 'styleguide', 'vendorstyles', 'images', 'markup', 'html', 'browserify', 'server', cb);
 });
