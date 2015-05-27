c7c2674455a2a3fa3c3909944f5406bdf1d19b1c
pyryjook <pyry@monday.fi>
2015-05-27 23:04:45 +0300

    add build and deploy tasks

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,6 +1,14 @@
 var gulp 				= require('gulp');
 var runSequence 		= require('run-sequence');
 
-gulp.task('deploy', function(cb){
+gulp.task('build', function(cb){
    runSequence('iconFont', 'favicons', 'sass', 'styleguide', 'vendorstyles', 'images', 'markup', 'html', 'browserify', 'server', cb);
+});
+
+gulp.task('deploy', function(cb){
+   runSequence('server', cb);
+});
+
+gulp.task('build-and-deploy', function(cb){
+   runSequence('build', 'deploy', cb);
 });
