41e10925b29d808b323557fbd49429d0023ba606
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2015-06-05 22:34:18 +0300

    Range-slider does something

--- source/js/pages/details4.js
+++ source/js/pages/details4.js
@@ -1,8 +1,15 @@
 var PageView = require('./base');
 var details = require('../templates/details4.hbs');
-
+var dom = require('ampersand-dom');
 
 module.exports = PageView.extend({
     pageTitle: 'Jalo',
-    template: details
+    template: details,
+    events: {
+      'change [data-hook=rangeslider]' : 'slided'
+    },
+    slided: function(event) {
+      var element = document.getElementById('ja-rangeslider__value');
+      dom.text(element, event.delegateTarget.value);
+    }
 });
